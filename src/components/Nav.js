21b701831f0b5e0c9d0b1f01f9adbb87aff8b83c
Olvine23 <olivinegeorge@gmal.com>
2022-03-12 21:59:45 -0800

    changed sth

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -225,7 +225,7 @@ export const Nav = ({pageTitle}) => {
                         <li>
                           <a
                             href="/"
-                            classame="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-green-500 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                            classame="inline-flex items-center justify-centegr w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-green-500 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                             aria-label="Sign up"
                             title="Sign up"
                           >

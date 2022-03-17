95403697142ba8029ab7247633f32caf35124570
Olvine23 <olivinegeorge@gmal.com>
2022-03-16 18:53:24 -0700

    changed the blog posts page layout

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,28 +2,33 @@ import React from 'react'
 import { graphql,Link } from 'gatsby'
 import Layout from '../../components/layout'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import Img from 'gatsby-image'
 
 const BlogPage = ({data}) => {
+  console.log(data)
+  const project = data.allMdx.nodes
+  
  
   return (
      <>
      <Layout pageTitle="My Blogs" />
+     
 
 
      {data.allMdx.nodes.map(
          node => (
              <article key ={node.id}>
 
-<div className="px-4   mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8  ">
-      <div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+<div className="px-4 flex mt-1  mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8  ">
+      <div className="  sm:max-w-sm sm:mx-auto lg:max-w-full">
         <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
           <Link
            to= {`/blog/${node.slug}`}
             >
-          <img
-            src="https://images.pexels.com/photos/2408666/pexels-photo-2408666.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+          <Img
+            fluid= {node.frontmatter.hero_image.childImageSharp.fluid}
             className="object-cover w-full h-64"
-            alt=""
+            alt="hero-image"
           />
           </Link>
           <div className="p-5 border border-t-0">
@@ -59,95 +64,12 @@ const BlogPage = ({data}) => {
             </Link>
           </div>
         </div>
-        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
-          <img
-            src="https://images.pexels.com/photos/447592/pexels-photo-447592.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-            className="object-cover w-full h-64"
-            alt=""
-          />
-          <div className="p-5 border border-t-0">
-            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
-              <a
-                href="/"
-                className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
-                aria-label="Category"
-                title="traveling"
-              >
-                traveling
-              </a>
-              <span className="text-gray-600">— 28 Dec 2020</span>
-            </p>
-            <a
-              href="/"
-              aria-label="Category"
-              title="Simple is better"
-              className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              Simple is better
-            </a>
-            <p className="mb-2 text-gray-700">
-              Sed ut perspiciatis unde omnis iste natus error sit sed quia
-              consequuntur magni voluptatem doloremque.
-            </p>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-            >
-              Learn more
-            </a>
-          </div>
-        </div>
-        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
-          <img
-            src="https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-            className="object-cover w-full h-64"
-            alt=""
-          />
-          <div className="p-5 border border-t-0">
-            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
-              <a
-                href="/"
-                className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
-                aria-label="Category"
-                title="traveling"
-              >
-                traveling
-              </a>
-              <span className="text-gray-600">— 28 Dec 2020</span>
-            </p>
-            <a
-              href="/"
-              aria-label="Category"
-              title="Film It!"
-              className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              Film It!
-            </a>
-            <p className="mb-2 text-gray-700">
-              Sed ut perspiciatis unde omnis iste natus error sit sed quia
-              consequuntur magni voluptatem doloremque.
-            </p>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-            >
-              Learn more
-            </a>
-          </div>
-        </div>
-      </div>
+    
     </div>
       
-                <h2 className="2xl">
-                  
-                 <Link to={`/blog/${node.slug}`}>  {node.frontmatter.title}  </Link>
-                   
-                   </h2>
-                <p className="text-gray-200 text-lg">{node.frontmatter.date}</p>
+                
                
-
+</div>
              </article>
          )
      )}
@@ -157,24 +79,30 @@ const BlogPage = ({data}) => {
 }
 
 export const query = graphql`
-  query {
-    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
-      nodes {
-        frontmatter {
-          date(formatString: "MMMM D, YYYY")
-          title
-          author
-          hero_image {
-            childImageSharp {
-              gatsbyImageData
+query {
+    
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      frontmatter {
+        date(formatString: "MMMM D, YYYY")
+        title
+        author
+        hero_image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            
             }
           }
         }
-        id
-       slug
       }
+      id
+      slug
     }
   }
+}
+
+
   `
 
 export default BlogPage;
